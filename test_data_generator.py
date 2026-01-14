e2cedb73d656664217e1baac95132594d0e74c86
Arthur <arthur.santos0202@outlook.com>
2026-01-13 23:36:18 -0300

    select specific test data from generated dataset

--- test_data_generator.py
+++ test_data_generator.py
@@ -2,4 +2,7 @@ def generate_job_titles():
     return ["QA Automation", "Python", "ServiceNow"]
 
 titles = generate_job_titles()
-print(titles)
+selected_title = titles[1]
+# Selecting a single job title to represent the test scenario
+
+print(selected_title)
